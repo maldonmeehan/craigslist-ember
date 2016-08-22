93d6d71387181f142b644bb80ea3ce618444d506
Charles Ewel and Maldon Meehan <charles+maldon@>
2016-08-22 09:33:19 -0700

    add form for creating new listings

---
+++ app/components/new-listing.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewListing: false,
+  actions: {
+    listingFormShow() {
+      this.set('addNewListing', true);
+    },
+
+    saveListing() {
+      var params = {
+        title: this.get('title'),
+        details: this.get('details'),
+        image: this.get('image') ? this.get('image'):"http://kingofwallpapers.com/house/house-026.jpg",
+        location: this.get('location'),
+        cost: parseInt(this.get('cost'))
+      };
+      this.set('addNewListing', false);
+      this.sendAction('saveListing', params);
+    }
+  }
+});
--- app/routes/category.js
+++ app/routes/category.js
@@ -6,5 +6,12 @@ export default Ember.Route.extend({
       category: this.store.findRecord('category', params.category_id),
       listings: this.store.findAll('listing')
     });
+  },
+
+  actions: {
+    saveListing(params) {
+      var newListing = this.store.createRecord('listing', params);
+      newListing.save();
+    }
   }
 });
