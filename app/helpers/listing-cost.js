91c9c2f5e97d0c66c115a4f648fe57088c6505fa
Charles Ewel and Maldon Meehan <charles+maldon@>
2016-08-22 14:32:15 -0700

    add all listings display

--- app/helpers/listing-cost.js
+++ app/helpers/listing-cost.js
@@ -1,25 +1,13 @@
 import Ember from 'ember';
 
 export function listingCost(params) {
-  var listingCategory = params[0].get('category').get('id');
-  if(listingCategory==="0") {
-    var carPrice = params[0].get('cost');
-    if (carPrice >= 4001){
-      return '$$$';
-    } else if (carPrice >= 2001){
-      return '$$';
-    } else {
-      return '$';
-    }
-  } else if (listingCategory==="1") {
-    var housePrice = params[0].get('cost');
-    if (housePrice >= 2001){
-      return '$$$';
-    } else if (housePrice >= 1001){
-      return '$$';
-    } else {
-      return '$';
-    }
+  var listingPrice = params[0].get('cost');
+  if (listingPrice > params[0].get('category').get('expensiveCutoff')){
+    return '$$$';
+  } else if (listingPrice > params[0].get('category').get('cheapCutoff')){
+    return '$$';
+  } else {
+    return '$';
   }
 }
 
--- app/models/category.js
+++ app/models/category.js
@@ -2,5 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
     name: DS.attr(),
+    expensiveCutoff: DS.attr(),
+    cheapCutoff: DS.attr(),
     listings: DS.hasMany('listing', {async: true})
 });
